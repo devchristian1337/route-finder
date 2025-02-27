7eb726c4ab15d0f25a98af6b006f0d7ca5bbe5a0
Christian Hannachi <hannachichristian02@gmail.com>
2025-02-27 12:59:32 +0100

    Fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * Combines multiple class names using clsx and then merges Tailwind classes
+ * with tailwind-merge to handle conflicts correctly
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
